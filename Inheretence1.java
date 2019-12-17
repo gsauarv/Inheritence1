4307ff0555530037f87055d0a2ff86a1def7f71f
gsauarv <gsaurav2000@gmail.com>
2019-12-17 09:36:05 +0545

    Sample Code

---
+++ Inheretence1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Inheretence1 extends inheritance2
+{
+   
+   public static void main(String[]args)
+   {
+       Inheretence1 obj = new Inheretence1();
+       obj.sum();                                                                                                                                              
+       
+       
+   }
+}
---
+++ inheritance2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class inheritance2
+{
+    void sum()
+    {   
+        int a=5;
+        a=a+a;
+        System.out.println("Sum is :  "  + a);
+    }
+    public static void main(String[]args)
+    {
+    }
+        
+}
